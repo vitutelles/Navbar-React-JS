749ae0a9cf09f28f0d524be825ffe622ce47ae25
Victor Telles <victortelles18@gmail.com>
2024-07-07 21:39:06 -0300

    feat:first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <header className="header"> 
+        <a href="/" className="logo">Logo</a>
+
+    <nav className="navbar">
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Portfolio</a>
+        <a href="/">Services</a>
+        <a href="/">Contact</a>
+        </nav>
+        
+    
+</header>
+  )
+}
+export default Navbar
